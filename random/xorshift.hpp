e44235d23a4581f39ad9f52dae7685f8983c994c
theory-and-me <akagiyasunori@akagiyasunorinoMacBook-Pro.local>
2023-06-22 00:16:57 +0900

    add annealing

---
+++ random/xorshift.hpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+uint32_t rand_int() // XorShift random integer generator
+{
+    static uint32_t x = 123456789, y = 362436069, z = 521288629, w = 88675123;
+    uint32_t t = x ^ (x << 11);
+    x = y;
+    y = z;
+    z = w;
+    return w = (w ^ (w >> 19)) ^ (t ^ (t >> 8));
+}
+double rand_double() { return (double)rand_int() / UINT32_MAX; }
